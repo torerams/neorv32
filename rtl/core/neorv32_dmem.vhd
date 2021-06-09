e495925f6a383706ea78741ec2f997f0a61da90c
stnolting <stnolting@gmail.com>
2021-06-09 18:21:54 +0200

    revert

--- rtl/core/neorv32_dmem.vhd
+++ rtl/core/neorv32_dmem.vhd
@@ -60,115 +60,81 @@ architecture neorv32_dmem_rtl of neorv32_dmem is
 
   -- IO space: module base address --
   constant hi_abb_c : natural := 31; -- high address boundary bit
-  constant lo_abb_c : natural := index_size_f(2048); -- low address boundary bit
+  constant lo_abb_c : natural := index_size_f(DMEM_SIZE); -- low address boundary bit
 
   -- local signals --
   signal acc_en : std_ulogic;
   signal rdata  : std_ulogic_vector(31 downto 0);
   signal rden   : std_ulogic;
-  signal addr   : std_ulogic_vector(index_size_f(2048/4)-1 downto 0);
+  signal addr   : std_ulogic_vector(index_size_f(DMEM_SIZE/4)-1 downto 0);
 
   -- RAM --
   -- The memory is built from 4x byte-wide memories defined as unique signals, since many synthesis tools
   -- have problems with 32-bit memories with byte-enable signals or with multi-dimensional arrays.
-  type dmem_file_t is array (2048/4-1 downto 0) of std_ulogic_vector(07 downto 0);
-  signal dmem_file_ll    : dmem_file_t;
-  signal dmem_file_lh    : dmem_file_t;
-  signal dmem_file_hl    : dmem_file_t;
-  signal dmem_file_hh    : dmem_file_t;
-  signal dmem_file_ll_rd : std_ulogic_vector(07 downto 0);
-  signal dmem_file_lh_rd : std_ulogic_vector(07 downto 0);
-  signal dmem_file_hl_rd : std_ulogic_vector(07 downto 0);
-  signal dmem_file_hh_rd : std_ulogic_vector(07 downto 0);
+  type dmem_file_t is array (0 to DMEM_SIZE/4-1) of std_ulogic_vector(7 downto 0);
+  signal dmem_file_ll : dmem_file_t;
+  signal dmem_file_lh : dmem_file_t;
+  signal dmem_file_hl : dmem_file_t;
+  signal dmem_file_hh : dmem_file_t;
+
 
   -- -------------------------------------------------------------------------------- --
   -- attributes - these are *NOT mandatory*; just for footprint / timing optimization --
   -- -------------------------------------------------------------------------------- --
 
----- lattice radiant --
---attribute syn_ramstyle : string;
---attribute syn_ramstyle of dmem_file_ll : signal is "no_rw_check";
---attribute syn_ramstyle of dmem_file_lh : signal is "no_rw_check";
---attribute syn_ramstyle of dmem_file_hl : signal is "no_rw_check";
---attribute syn_ramstyle of dmem_file_hh : signal is "no_rw_check";
---
----- intel quartus prime --
---attribute ramstyle : string;
---attribute ramstyle of dmem_file_ll : signal is "no_rw_check";
---attribute ramstyle of dmem_file_lh : signal is "no_rw_check";
---attribute ramstyle of dmem_file_hl : signal is "no_rw_check";
---attribute ramstyle of dmem_file_hh : signal is "no_rw_check";
+  -- lattice radiant --
+  attribute syn_ramstyle : string;
+  attribute syn_ramstyle of dmem_file_ll : signal is "no_rw_check";
+  attribute syn_ramstyle of dmem_file_lh : signal is "no_rw_check";
+  attribute syn_ramstyle of dmem_file_hl : signal is "no_rw_check";
+  attribute syn_ramstyle of dmem_file_hh : signal is "no_rw_check";
+
+  -- intel quartus prime --
+  attribute ramstyle : string;
+  attribute ramstyle of dmem_file_ll : signal is "no_rw_check";
+  attribute ramstyle of dmem_file_lh : signal is "no_rw_check";
+  attribute ramstyle of dmem_file_hl : signal is "no_rw_check";
+  attribute ramstyle of dmem_file_hh : signal is "no_rw_check";
 
 begin
 
   -- Access Control -------------------------------------------------------------------------
   -- -------------------------------------------------------------------------------------------
   acc_en <= '1' when (addr_i(hi_abb_c downto lo_abb_c) = DMEM_BASE(hi_abb_c downto lo_abb_c)) else '0';
-  addr   <= addr_i(index_size_f(2048/4)+1 downto 2); -- word aligned
+  addr   <= addr_i(index_size_f(DMEM_SIZE/4)+1 downto 2); -- word aligned
 
 
   -- Memory Access --------------------------------------------------------------------------
   -- -------------------------------------------------------------------------------------------
-  dmem_file_access_b0: process(clk_i)
-  begin
-    if rising_edge(clk_i) then
---    if (acc_en = '1') then -- reduce switching activity when not accessed
-        if (wren_i = '1') and (ben_i(0) = '1') then
-          dmem_file_ll(to_integer(unsigned(addr))) <= data_i(07 downto 00);
-        end if;
-        dmem_file_ll_rd <= dmem_file_ll(to_integer(unsigned(addr)));
---    end if;
-    end if;
-  end process dmem_file_access_b0;
-
-  dmem_file_access_b1: process(clk_i)
-  begin
-    if rising_edge(clk_i) then
---    if (acc_en = '1') then -- reduce switching activity when not accessed
-        if (wren_i = '1') and (ben_i(0) = '1') then
-          dmem_file_lh(to_integer(unsigned(addr))) <= data_i(07 downto 00);
-        end if;
-        dmem_file_lh_rd <= dmem_file_lh(to_integer(unsigned(addr)));
---    end if;
-    end if;
-  end process dmem_file_access_b1;
-
-  dmem_file_access_b2: process(clk_i)
-  begin
-    if rising_edge(clk_i) then
---    if (acc_en = '1') then -- reduce switching activity when not accessed
-        if (wren_i = '1') and (ben_i(0) = '1') then
-          dmem_file_hl(to_integer(unsigned(addr))) <= data_i(07 downto 00);
-        end if;
-        dmem_file_hl_rd <= dmem_file_hl(to_integer(unsigned(addr)));
---    end if;
-    end if;
-  end process dmem_file_access_b2;
-
-  dmem_file_access_b3: process(clk_i)
-  begin
-    if rising_edge(clk_i) then
---    if (acc_en = '1') then -- reduce switching activity when not accessed
-        if (wren_i = '1') and (ben_i(0) = '1') then
-          dmem_file_hh(to_integer(unsigned(addr))) <= data_i(07 downto 00);
-        end if;
-        dmem_file_hh_rd <= dmem_file_hh(to_integer(unsigned(addr)));
---    end if;
-    end if;
-  end process dmem_file_access_b3;
-
-  rdata <= dmem_file_hh_rd & dmem_file_hl_rd & dmem_file_lh_rd & dmem_file_ll_rd;
-
-
-  -- Bus Feedback ---------------------------------------------------------------------------
-  -- -------------------------------------------------------------------------------------------
-  bus_feedback: process(clk_i)
+  dmem_file_access: process(clk_i)
   begin
     if rising_edge(clk_i) then
       rden  <= rden_i and acc_en;
       ack_o <= acc_en and (rden_i or wren_i);
+      if (acc_en = '1') then -- reduce switching activity when not accessed
+        -- write --
+        if (wren_i = '1') then
+          if (ben_i(0) = '1') then
+            dmem_file_ll(to_integer(unsigned(addr))) <= data_i(07 downto 00);
+          end if;
+          if (ben_i(1) = '1') then
+            dmem_file_lh(to_integer(unsigned(addr))) <= data_i(15 downto 08);
+          end if;
+          if (ben_i(2) = '1') then
+            dmem_file_hl(to_integer(unsigned(addr))) <= data_i(23 downto 16);
+          end if;
+          if (ben_i(3) = '1') then
+            dmem_file_hh(to_integer(unsigned(addr))) <= data_i(31 downto 24);
+          end if;
+        end if;
+        -- read --
+        rdata(07 downto 00) <= dmem_file_ll(to_integer(unsigned(addr)));
+        rdata(15 downto 08) <= dmem_file_lh(to_integer(unsigned(addr)));
+        rdata(23 downto 16) <= dmem_file_hl(to_integer(unsigned(addr)));
+        rdata(31 downto 24) <= dmem_file_hh(to_integer(unsigned(addr)));
+      end if;
     end if;
-  end process bus_feedback;
+  end process dmem_file_access;
 
   -- output gate --
   data_o <= rdata when (rden = '1') else (others => '0');
