40d4d09f7e21ee4ce9e38ebf263396be0c44623a
Tore Ramsland <tore.ramsland@firmwaredesign.no>
2021-08-04 22:30:39 +0200

    Removed wishbone2avalonmm hack

--- rtl/templates/neorv32_qsys_component/wishbone2avalonmm.vhd
+++ rtl/templates/neorv32_qsys_component/wishbone2avalonmm.vhd
@@ -35,70 +35,14 @@ end wishbone2avalonmm;
 architecture rtl of wishbone2avalonmm is
 ----------------------------------------------------------------------------------------------------
 
-attribute KEEP		: string;
-attribute PRESERVE  : string;
-attribute NOPRUNE   : string;
-
-signal  read_int            : std_logic;
-signal  write_int           : std_logic;
-
-signal  wb_adr              : std_logic_vector(31 downto 0);
-signal  wb_dat              : std_logic_vector(31 downto 0);
-signal  wb_stb              : std_logic;
-signal  wb_we               : std_logic;
-signal  wb_ack              : std_logic;
-signal  read_smp            : std_logic;
-signal  write_smp           : std_logic;
-
-attribute KEEP OF wb_adr:SIGNAL is "TRUE";
-attribute KEEP OF wb_dat:SIGNAL is "TRUE";
-attribute KEEP OF wb_stb:SIGNAL is "TRUE";
-attribute KEEP OF wb_we:SIGNAL is "TRUE";
-attribute KEEP OF wb_ack:SIGNAL is "TRUE";
-attribute KEEP OF wb_dat_o:SIGNAL is "TRUE";
-attribute PRESERVE OF wb_adr:SIGNAL is "TRUE";
-attribute PRESERVE OF wb_dat:SIGNAL is "TRUE";
-attribute PRESERVE OF wb_stb:SIGNAL is "TRUE";
-attribute PRESERVE OF wb_we:SIGNAL is "TRUE";
-attribute PRESERVE OF wb_ack:SIGNAL is "TRUE";
-attribute PRESERVE OF wb_dat_o:SIGNAL is "TRUE";
-attribute NOPRUNE OF wb_adr:SIGNAL is "TRUE";
-attribute NOPRUNE OF wb_dat:SIGNAL is "TRUE";
-attribute NOPRUNE OF wb_stb:SIGNAL is "TRUE";
-attribute NOPRUNE OF wb_we:SIGNAL is "TRUE";
-attribute NOPRUNE OF wb_ack:SIGNAL is "TRUE";
-attribute NOPRUNE OF wb_dat_o:SIGNAL is "TRUE";
-
 begin
 
-    read <= read_int;
-    write <= write_int;
-
-    process(clk)
-    begin
-        if rising_edge(clk) then
-            wb_adr <= std_logic_vector(wb_adr_i);
-            wb_dat <= std_logic_vector(wb_dat_i);
-            wb_stb <= wb_stb_i;
-            wb_we <= wb_we_i;
-
-            --wb_dat_o <= std_ulogic_vector(wb_adr or wb_dat or (x"000000" & "000" & wb_ack & read_smp & write_smp & wb_stb & wb_we));
-
-            wb_ack <= not(waitrequest);
-
-            read_smp <= read_int;
-            write_smp <= write_int;
-
-        end if;
-    end process;
-
-    read_int <= '1' when (wb_stb_i = '1' and wb_we_i = '0') else '0';
-    write_int <= '1' when (wb_stb_i = '1' and wb_we_i = '1') else '0';
-    address <= x"0" & std_logic_vector(wb_adr_i(27 downto 0));
+    read <= '1' when (wb_stb_i = '1' and wb_we_i = '0') else '0';
+    write <= '1' when (wb_stb_i = '1' and wb_we_i = '1') else '0';
+    address <= std_logic_vector(wb_adr_i);
     writedata <= std_logic_vector(wb_dat_i);
 
     wb_dat_o <= std_ulogic_vector(readdata);
-    --wb_dat_o <= x"12345678";
     wb_ack_o <= not(waitrequest);
     wb_err_o <= '0';
 
